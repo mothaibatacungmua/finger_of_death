47f76dff84c94ce2a6c96938d8990cfc41fbd2fa
ducta-tiki <duc.tran3@tiki.vn>
2019-10-09 18:06:56 +0700

    Update dcgan.cpp

--- dcgan/dcgan.cpp
+++ dcgan/dcgan.cpp
@@ -1,41 +1,99 @@
 #include <torch/torch.h>
 #include <iostream>
-#include <string>
+#include <cstdio>
+#include <cmath>
 
-using namespace std;
+using namespace torch;
 // https://pytorch.org/cppdocs/api/namespace_torch__nn.html
 
-struct Net : torch::nn::Module {
-    Net(int64_t N, int64_t M) 
-        : linear(register_module("linear", torch::nn::Linear(N, M))){
-        another_bias = register_parameter("b", torch::randn(M));
+
+struct GeneratorImpl : nn::Module {
+    GeneratorImpl (int kNoiseSize)
+        : conv1(nn::Conv2dOptions(kNoiseSize, 256, 4)
+                    .with_bias(false)
+                    .transposed(true)),
+          batch_norm1(256),
+          conv2(nn::Conv2dOptions(256, 128, 3)
+                    .stride(2)
+                    .padding(1)
+                    .with_bias(false)
+                    .transposed(true)),
+          batch_norm2(128),
+          conv3(nn::Conv2dOptions(128, 64, 4)
+                    .stride(2)
+                    .padding(1)
+                    .with_bias(false)
+                    .transposed(true)),
+          batch_norm3(64),
+          conv4(nn::Conv2dOptions(64, 1, 4)
+                    .stride(2)
+                    .padding(1)
+                    .with_bias(false)
+                    .transposed(true))
+
+    {
+        register_module("conv1", conv1);
+        register_module("conv2", conv2);
+        register_module("conv3", conv3);
+        register_module("conv4", conv4);
+        register_module("batch_norm1", batch_norm1);
+        register_module("batch_norm2", batch_norm2);
+        register_module("batch_norm3", batch_norm3);
     }
 
-    torch::Tensor forward(torch::Tensor input){
-        return linear(input) + another_bias;
+    torch::Tensor forward(torch::Tensor x){
+        x = torch::relu(batch_norm1(conv1(x)));
+        x = torch::relu(batch_norm2(conv2(x)));
+        x = torch::relu(batch_norm3(conv3(x)));
+        x = torch::relu(conv4(x));
+        return x;
     }
-    torch::nn::Linear linear;
-    torch::Tensor another_bias;
+
+    nn::Conv2d conv1, conv2, conv3, conv4;
+    nn::BatchNorm batch_norm1, batch_norm2, batch_norm3;
 };
 
-string get_name(){
-    return "Alex";
-}
+TORCH_MODULE(Generator);
 
-void print_ref_1(const string& name){
-    cout << name;
-}
 
-void print_ref_2(string&& name){
-    cout << name;
-}
+struct DiscriminatorImpl : nn::Module {
+    DiscriminatorImpl()
+        : conv1(nn::Conv2dOptions(1, 64, 4)
+                    .stride(2)
+                    .padding(1)
+                    .with_bias(false)),
+          batch_norm1(64),
+          conv2(nn::Conv2dOptions(64, 128, 4)
+                    .stride(2)
+                    .padding(1)
+                    .with_bias(false)),
+          batch_norm2(128),
+          conv3(nn::Conv2dOptions(128, 256, 4)
+                    .stride(2)
+                    .padding(1)
+                    .with_bias(false)),
+          batch_norm3(256),
+          conv4(nn::Conv2dOptions(256, 1, 3)
+                    .stride(1)
+                    .padding(0)
+                    .with_bias(false))
+    {
+        register_module("conv1", conv1);
+        register_module("conv2", conv2);
+        register_module("conv3", conv3);
+        register_module("conv4", conv4);
+        register_module("batch_norm1", batch_norm1);
+        register_module("batch_norm2", batch_norm2);
+        register_module("batch_norm3", batch_norm3);
+    };
+
+    nn::Conv2d conv1, conv2, conv3, conv4;
+    nn::BatchNorm batch_norm1, batch_norm2, batch_norm3;
+        
+};
+TORCH_MODULE(Discriminator);
+
 
 int main(){
-    // Net net(4, 5);
-    // for(const auto& p: net.parameters()){
-    //     std::cout << p << std::endl;
-    // }
-
-    string me("Alex");
-    print_ref_2(me);
+    return 0;
 }
